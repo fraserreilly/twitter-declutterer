4bd672ad6bb5849a4a4100ee36f698c5a7141747
Fraser Reilly <26841319+fraserreilly@users.noreply.github.com>
2022-08-31 20:23:58 +0100

    Update declutter.user.js

--- declutter.user.js
+++ declutter.user.js
@@ -3,15 +3,15 @@
 // @namespace    https://github.com/fraserreilly
 // @version      1.0
 // @license      GNU General Public License v3.0
-// @description  Removes the sidebar column from twitter (right sidebar).
+// @description  Removes the sidebar column from twitter (right sidebar) and dm drawer.
 // @author       fraserreilly
 // @match        *://twitter.com/*
 // @exclude      *//twitter.com/i/flow/login
 // @grant        none
-// @homepageURL  https://github.com/fraserreilly/twitterDeclutter
-// @supportURL   https://github.com/fraserreilly/twitterDeclutter/issues
-// @downloadURL  https://raw.githubusercontent.com/fraserreilly/twitterDeclutter/declutter.user.js
-// @updateURL    https://raw.githubusercontent.com/fraserreilly/twitterDeclutter/declutter.user.js
+// @homepageURL  https://github.com/fraserreilly/twitter-declutterer
+// @supportURL   https://github.com/fraserreilly/twitter-declutterer/issues
+// @downloadURL  https://raw.githubusercontent.com/fraserreilly/twitter-declutterer/declutter.user.js
+// @updateURL    https://raw.githubusercontent.com/fraserreilly/twitter-declutterer/declutter.user.js
 // ==/UserScript==
 
 function waitForElm(selector) {
